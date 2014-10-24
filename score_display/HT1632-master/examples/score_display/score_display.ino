beb53057b86f788424a9d8645d7521eae8fd1c31
Vladan Nikolic <vladann@kth.se>
2014-10-24 16:49:21 +0200

    Add end_match method, tunes, and shiet.

--- score_display/HT1632-master/examples/score_display/score_display.ino
+++ score_display/HT1632-master/examples/score_display/score_display.ino
@@ -1,10 +1,11 @@
 #include "HT1632.h"
+#include <stdlib.h>
 
 #define DATA 2
 #define WR 3
 #define CS1 4
 #define CS2 5
-#define CS3 13
+#define SOUND_PIN 13
 
 #define SCREEN_WIDTH 48
 #define SCREEN_HEIGHT 16
@@ -19,25 +20,48 @@ void setup() {
   matrix.setTextColor(1);   // 'lit' LEDs
 
   //set_score(1, 1); // kolla om det är tvåsiffrigt
-  //start_match();
+  start_match();
   score_goal(1,0);
+  end_match(false);
 }
 
 void loop(){}
 
-void make_sick_noise() {
-  scale(CS3);
-  for(int i=0;i<1000;i++){
-    digitalWrite(CS3, HIGH);
-    delay(1);
-    digitalWrite(CS3, LOW);
-  }  
-}
+
 
 void start_match() {
   create_explosions(); 
   countdown();
   set_score(0,0);
+}
+
+void end_match(boolean playerOneWon) {
+  matrix.setBrightness(15);
+  matrix.setTextSize(1);
+  
+  for(int i = SCREEN_WIDTH; i >= 3; --i) {
+    //matrix.drawChar(i, 1, 'L', 1, 2);
+    if (playerOneWon) {
+      matrix.setCursor(i, 1);
+      matrix.print("<- 1337");
+      matrix.setCursor(i, 9);
+      matrix.print("N00B ->");
+    } else {
+      matrix.setCursor(i, 1);
+      matrix.print("1337 ->");
+      matrix.setCursor(i, 9);
+      matrix.print("<- N00B");
+    }
+    matrix.writeScreen();
+    delay(20);
+    if (i > 3)
+      matrix.clearScreen();
+  }
+  
+  for (int i = 0; i < 2; ++i) {
+    fadeOut();
+    fadeIn();
+  }
 }
 
 void countdown() {
@@ -69,9 +93,9 @@ void countdown() {
 
 void create_explosions() {
   create_explosion(24, 8, 25, 1);
-  create_explosion(5, 5, 5, 0);
   create_explosion(13, 7, 4, 0);
   create_explosion(41, 8, 10, 0);
+  create_explosion(5, 5, 5, 0);
   create_explosion(15, 7, 20, 0);
 }
 
@@ -85,21 +109,25 @@ void create_explosion(int8_t x, int8_t y, int8_t max_radius, uint8_t brightness)
 
 void set_score(int scoreX, int scoreY) {
   matrix.clearScreen();
-  matrix.setBrightness(15);
   
+  matrix.setBrightness(15);
   matrix.setCursor(7, 1);   // start at top left, with one pixel of spacing
   matrix.print(scoreX);
+  
   matrix.setCursor(19,1);
   matrix.print("-");
+  
   matrix.setCursor(31, 1);   // next line, 8 pixels down
   matrix.print(scoreY);
   matrix.writeScreen();
-  delay(2000);
+  
 }
 
 
 void score_goal(int scoreX, int scoreY) {
-  make_sick_noise();
+  
+  make_some_noise();
+  
   matrix.setBrightness(15);
   
   for(int i = SCREEN_WIDTH; i >= 1; --i) {
@@ -119,7 +147,11 @@ void score_goal(int scoreX, int scoreY) {
   fadeOut();
   
   set_score(scoreX, scoreY);
+  
+  play_tune();
 }
+
+
 
 void fadeOut() {
   for (int8_t i = 15; i >= 0; --i) {
@@ -135,41 +167,65 @@ void fadeIn() {
   }
 }
 
-void beep (unsigned char speakerPin, int frequencyInHertz, long timeInMilliseconds)     // the sound producing function
-{ 	 
-          int x; 	 
-          long delayAmount = (long)(1000000/frequencyInHertz);
-          long loopTime = (long)((timeInMilliseconds*1000)/(delayAmount*2));
-          for (x=0;x<loopTime;x++) 	 
-          { 	 
-              digitalWrite(speakerPin,HIGH);
-              delayMicroseconds(delayAmount);
-              digitalWrite(speakerPin,LOW);
-              delayMicroseconds(delayAmount);
-          } 	 
-} 	 
+void play_tune() {
+  int numMelodies = 1;
+
+  switch(rand() % numMelodies) {
+    case 0: seven_nation_army();
+  }
+}
+
+void make_some_noise() {
+  playNote('G', 125);
+  delay(125);
+  playNote('G', 125);
+  playNote('E', 125);
+  playNote('G', 500);
+}
+	 
   	 
-void scale (int speakerPin)
-{ 	  
-         playNote('E', 500);
-         delay(250);
-         playNote('E', 250);
-         playNote('G', 375);
-         delay(125);
-         playNote('E', 250);
-         playNote('D', 250);
-         playNote('C', 1000);
-         playNote('B', 1000);
+void seven_nation_army() { 	  
+  playNote('E', 500);
+  delay(250);
+  playNote('E', 250);
+  playNote('G', 333);
+  playNote('E', 333);
+  playNote('D', 332);
+  playNote('C', 1000);
+  playNote('B', 500);
+  delay(500);
+         
+  playNote('E', 500);
+  delay(250);
+  playNote('E', 250);
+  playNote('G', 333);
+  playNote('E', 333);
+  playNote('D', 332);
+  playNote('C', 1000);
+  playNote('B', 500);
+  delay(500);
 }
 
 void playNote(char note, int to_delay) {
   switch(note){
-    case 'A': beep(CS3,3520,to_delay); break;
-    case 'B': beep(CS3,1976,to_delay); break;
-    case 'C': beep(CS3,2093,to_delay); break;
-    case 'D': beep(CS3,2349,to_delay); break;
-    case 'E': beep(CS3,2637,to_delay); break;
-    case 'F': beep(CS3,2793,to_delay); break;
-    case 'G': beep(CS3,3136,to_delay); break;
+    case 'A': beep(SOUND_PIN,3520,to_delay); break;
+    case 'B': beep(SOUND_PIN,1976,to_delay); break;
+    case 'C': beep(SOUND_PIN,2093,to_delay); break;
+    case 'D': beep(SOUND_PIN,2349,to_delay); break;
+    case 'E': beep(SOUND_PIN,2637,to_delay); break;
+    case 'F': beep(SOUND_PIN,2793,to_delay); break;
+    case 'G': beep(SOUND_PIN,3136,to_delay); break;
   }
 }
+
+
+void beep (unsigned char speakerPin, int frequencyInHertz, long timeInMilliseconds) { 	  	 
+  long delayAmount = (long)(1000000/frequencyInHertz);
+  long loopTime = (long)((timeInMilliseconds*1000)/(delayAmount*2));
+  for (int x = 0; x < loopTime; ++x) { 	 
+    digitalWrite(speakerPin,HIGH);
+    delayMicroseconds(delayAmount);
+    digitalWrite(speakerPin,LOW);
+    delayMicroseconds(delayAmount);
+  } 	 
+} 
